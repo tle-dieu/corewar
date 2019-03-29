95fb75657651b6045eb9e33169d576fa1c024c12
Thomas LE DIEU DE VILLE <tle-dieu@e3r12p4.42.fr>
2019-03-29 14:49:05 +0100

    ajout ressources, .gitignore mis a jour, script fonctionne

--- exec.sh
+++ exec.sh
@@ -1,6 +1,21 @@
+#!/bin/bash
+
 i=0
-for arg in $*; do
-	./asm $arg
-	mv $arg .
-done
-./corewar 
+if [[ $(grep -E '\-\-help|\-h' <<< $*) ]]; then
+	echo "usage: champ1 champ2 champ3 champ4 vm_option"
+else
+	for arg in $*; do
+		if [[ $i -ge 4 || ${arg:0:1} == '-' ]]; then
+			break
+		fi
+		./asm $arg
+		if [[ $? != 0 ]]; then
+			echo "ERROR" && exit
+		fi
+		champ[i]="$(basename "$arg" .s).cor"
+		mv -f ${champ[$i]} . 2>/dev/null
+		i=$(($i + 1))
+	done
+	echo "./corewar ${@:$((i + 1))} ${champ[*]}"
+	./corewar ${@:$((i + 1))} ${champ[*]}
+fi
