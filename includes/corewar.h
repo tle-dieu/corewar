75f4478cc2bb0700a39002b9bf2283b113d2e5f5
Matthew LEROY <matleroy@e3r6p14.42.fr>
2019-04-04 13:26:01 +0200

    ajout structures das corewar.h

--- includes/corewar.h
+++ includes/corewar.h
@@ -6,7 +6,7 @@
 /*   By: matleroy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 11:32:19 by matleroy          #+#    #+#             */
-/*   Updated: 2019/04/04 11:46:52 by matleroy         ###   ########.fr       */
+/*   Updated: 2019/04/04 13:25:26 by matleroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,28 @@
 # define COREWAR_H
 
 int	usage(int ac, char **av);
+
+typedef	struct	s_champ
+{
+	char			*name;
+	char			*desc;
+	int				id;
+	int				size;
+}				t_champ;
+
+typedef	struct	s_en
+{
+	int				c;
+	int				c_to_die;
+	int				c_total;
+	t_champ			*champs;
+	unsigned char **map;
+}				t_env;
+
+typedef struct s_inst
+{
+	int				opcode;
+	int				cycle;
+	struct s_inst	next;
+}				t_inst
 #endif
