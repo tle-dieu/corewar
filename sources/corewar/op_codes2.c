3bd11eb385e552777b0ab8770bcee1aa59ae681d
Matthew LEROY <matleroy@student.42.fr>
2019-04-09 20:26:26 +0200

    modification  sti et ldi

--- sources/corewar/op_codes2.c
+++ sources/corewar/op_codes2.c
@@ -3,104 +3,105 @@
 /*                                                        :::      ::::::::   */
 /*   op_codes2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acompagn <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: matleroy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/08 14:15:29 by acompagn          #+#    #+#             */
-/*   Updated: 2019/04/08 21:01:11 by matleroy         ###   ########.fr       */
+/*   Created: 2019/04/09 20:19:06 by matleroy          #+#    #+#             */
+/*   Updated: 2019/04/09 20:23:46 by matleroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-void		ldi(t_env *e, int *pc, t_proc *ptr)
+void insert(t_env *e, int pc, void *ptr, int size)
 {
-	t_ocp	check;
-	int sum;
-	int m;
-	int err;
-
-	err = 0;
-	m = 0;
-	sum = 0;
-	ft_printf("in ldi :: ");
-	check = check_ocp(e->mem[(*pc + 1) % MEM_SIZE], 1);
-	if (check.p1 && check.p2 && check.p3)
-	{
-		m = 1;
-		if (check.p3 == 64 && check.p2 <= 32)
-		{
-			if (check.p3 == 4)
-				sum += ptr->r[param_sum(e, (*pc + m) % MEM_SIZE, check.p3)];
-			else if (check.p3 > 4)
-				sum += e->mem[*pc + param_sum(e, (*pc + m) % MEM_SIZE, check.p3)];
-			else
-				sum += param_sum(e, (*pc + m) % MEM_SIZE, check.p3);
-			m += check.s3;
-			if (!err && check.p2 == 32)
-				sum += param_sum(e, (*pc + m) % MEM_SIZE, check.p2);
-			else if (!err && check.p2 > 32)
-				sum += e->mem[*pc + param_sum(e, (*pc + m) % MEM_SIZE, check.p2)];
-			else
-				err = 1;
-			if (!err && check.p1 == 64)
-				ptr->r[param_sum(e, *pc % MEM_SIZE, check.p1)] = sum;
-			else
-				err = 1;
-			ptr->carry = !sum;
-		}
-		move_pc(check, pc, 1);
-	}
+	char *tmp;
+	tmp = (char*)ptr;
+	while (size--)
+		e->mem[pc + size] = *tmp;
 }
 
-//fait
-
-void		sti(t_env *e, int *pc, t_proc *ptr)
+void        sti(t_env *e, int *pc, t_proc *ptr)
 {
 	t_ocp	check;
-	int sum;
-	int err;
-	int m;
+	int		sum;
+	int		err;
+	int		m;
 
 	err = 0;
 	m = 0;
 	sum = 0;
 	ft_printf("in sti :: ");
 	check = check_ocp(e->mem[(*pc + 1) % MEM_SIZE], 1);
-	if (check.p1 == 64 && check.p2  && check.p3 < 12 && check.p3)
+	if (check.p1 == 64 && check.p2  && check.p3 < 12)
 	{
-		ft_printf("OK ");
-		m = 1;
-		if (check.p2 == 4)
-			sum += ptr->r[param_sum(e, (*pc + m) % MEM_SIZE, check.p2)];
-		else if (check.p2 > 4)
-			sum += e->mem[*pc + param_sum(e, (*pc + m) % MEM_SIZE, check.p2)];
+		m = 2 + check.s1;
+		if (check.p2 == 16)
+			sum += ptr->r[param_sum(e, (*pc + m) % MEM_SIZE, check.s2)];
+		else if (check.p2 > 32)
+			sum += e->mem[*pc + (param_sum(e, (*pc + m) % MEM_SIZE, check.s2) % IDX_MOD)];
 		else
-			sum += param_sum(e, (*pc + m) % MEM_SIZE, check.p2);
-		if (!err && check.p3 == 32)
-			sum += param_sum(e, (*pc + m) % MEM_SIZE, check.p3);
-		else if (!err && check.p3 > 32)
-			sum += e->mem[*pc + param_sum(e, (*pc + m) % MEM_SIZE, check.p3)];
+			sum += param_sum(e, (*pc + m) % MEM_SIZE, check.s2);
+		m = 2 + check.s1 + check.s2;
+		if (!err && check.p3 == 8)
+			sum += param_sum(e, (*pc + m) % MEM_SIZE, check.s3);
+		else if (!err && check.p3 == 4)
+			sum += ptr->r[param_sum(e, (*pc + m) % MEM_SIZE, check.s3)];
 		else
 			err = 1;
-		if ((e->mem[*pc + m] = ptr->r[param_sum(e, *pc % MEM_SIZE, 1)]))
-			ptr->carry = 0;
-		else
-			ptr->carry = 1;
+		m = 2;
+		insert(e, (*pc + (sum %IDX_MOD)) %MEM_SIZE, (void*)&ptr->r[param_sum(e, (*pc + m) % MEM_SIZE, 1)], 4);
 	}
 	else
 		ft_printf("ERROR ");
-	move_pc(check, pc, 1);
+	*pc += 2 + check.s1 + check.s2 + check.s3;
 }
 
-void		op_fork(t_env *e, int *i, t_proc *ptr)
+void        ldi(t_env *e, int *pc, t_proc *ptr)
 {
-	ft_printf("in op_fork :: ");
+	t_ocp   check;
+	int sum;
+	int m;
+	int err;
+
+	err = 0;
+	sum = 0;
+	ft_printf("in ldi :: ");
+	check = check_ocp(e->mem[(*pc + 1) % MEM_SIZE], 1);
+	if (check.p3 == 4 && check.p2 <= 32 && check.p3)
+	{
+		m = 2;
+		if (check.p1 == 4)
+			sum += ptr->r[param_sum(e, (*pc + m) % MEM_SIZE, check.p1)];
+		else if (check.p1 > 8)
+			sum += e->mem[(*pc + param_sum(e, (*pc + m) % MEM_SIZE, IND_SIZE)) % MEM_SIZE];
+		else
+			sum += param_sum(e, (*pc + m) % MEM_SIZE, check.p3);
+		m = 2 + check.s1;
+		if (!err && check.p2 == 16)
+			sum += ptr->r[param_sum(e, (*pc + m) % MEM_SIZE, check.p2)];
+		else if (!err && check.p2 == 32)
+			sum += param_sum(e, (*pc + m) % MEM_SIZE, check.p2);
+		else
+			err = 1;
+		m = 2 + check.s1 + check.s2;
+		if (!err && check.p3 == 64)
+			ptr->r[param_sum(e, *pc % MEM_SIZE, check.p3)] = sum;
+		else
+			err = 1;
+		ptr->carry = !sum;
+	}
+	*pc += 2 + check.s1 + check.s2 + check.s3;
+}
+
+void        op_fork(t_env *e, int *i, t_proc *ptr)
+{
+	ft_printf("in fork :: ");
 	(void)e;
 	(void)i;
 	(void)ptr;
 }
 
-void		lld(t_env *e, int *i, t_proc *ptr)
+void        lld(t_env *e, int *i, t_proc *ptr)
 {
 	ft_printf("in lld :: ");
 	(void)e;
@@ -108,7 +109,7 @@ void		lld(t_env *e, int *i, t_proc *ptr)
 	(void)ptr;
 }
 
-void		lldi(t_env *e, int *i, t_proc *ptr)
+void        lldi(t_env *e, int *i, t_proc *ptr)
 {
 	ft_printf("in lldi :: ");
 	(void)e;
@@ -116,7 +117,7 @@ void		lldi(t_env *e, int *i, t_proc *ptr)
 	(void)ptr;
 }
 
-void		lfork(t_env *e, int *i, t_proc *ptr)
+void        lfork(t_env *e, int *i, t_proc *ptr)
 {
 	ft_printf("in lfork :: ");
 	(void)e;
@@ -124,10 +125,9 @@ void		lfork(t_env *e, int *i, t_proc *ptr)
 	(void)ptr;
 }
 
-// FAIT!
-void		aff(t_env *e, int *pc, t_proc *ptr)
+void        aff(t_env *e, int *pc, t_proc *ptr)
 {
-	t_ocp	check;
+	t_ocp   check;
 
 	ft_printf("in aff :: ");
 	check = check_ocp(e->mem[(*pc + 1) % MEM_SIZE], 0);
