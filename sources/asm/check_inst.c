728be61bb9d1ca68522082e64c23e007d5516ee8
Matthew LEROY <matleroy@student.42.fr>
2019-05-06 14:46:50 +0200

    modification check_params

--- sources/asm/check_inst.c
+++ sources/asm/check_inst.c
@@ -6,7 +6,7 @@
 /*   By: matleroy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/01 17:17:09 by matleroy          #+#    #+#             */
-/*   Updated: 2019/05/02 14:31:14 by matleroy         ###   ########.fr       */
+/*   Updated: 2019/05/06 14:44:11 by matleroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,18 @@ int		is_direct(t_env *e, char *str, t_inst *inst)
 		{
 			if (label_is_good(e, tmp + 1))
 				get_label_call(e, inst, tmp + 1, inst->i);
+			else
+				inst->error++;
 		}
 		else
 		{
 			if (is_a_number(e, tmp))
 				inst->p[inst->i] = inst_atoi(tmp);
 			else
+			{
+				inst->error++;
 				return (!(inst->t[inst->i] = 0));
+			}
 		}
 		return (1);
 	}
@@ -51,13 +56,18 @@ int		is_indirect(t_env *e, char *str, t_inst *inst)
 	{
 		if (label_is_good(e, tmp + 1))
 			get_label_call(e, inst, tmp + 1, inst->i);
+		else
+			inst->error++;
 	}
 	else
 	{
 		if (is_a_number(e, tmp))
 			inst->p[inst->i] = inst_atoi(tmp);
 		else
+		{
+			inst->error++;
 			return (inst->t[inst->i] = 0);
+		}
 	}
 	return (1);
 }
@@ -99,8 +109,13 @@ int		is_reg(t_env *e, char *str, t_inst *inst)
 		inst->t[inst->i] = 1;
 		if (is_valid_register(e, tmp))
 			inst->p[inst->i] = inst_atoi(tmp);
+		else
+			inst->error++;
 		if (inst->p[inst->i] < 0 || inst->p[inst->i] > REG_NUMBER)
+		{
 			error_register_nb(e, tmp, inst->p[inst->i]);
+			inst->error++;
+		}
 		return (1);
 	}
 	return (0);
@@ -116,25 +131,32 @@ int		check_params(t_env *e, char *str, t_inst *inst)
 	while (*tmp)
 	{
 		tmp += ft_strspn(tmp, SPACES);
-		if (g_op_tab[inst->op - 1].nb_param >= inst->i)
-			begin = tmp;
 		if (g_op_tab[inst->op - 1].nb_param > inst->i)
+		{
+			begin = tmp;
 			is_direct(e, tmp, inst) || is_reg(e, tmp, inst)
 			|| is_indirect(e, tmp, inst);
+			if (inst->t[inst->i])
+			{
+				error_param_type(e, inst, begin);
+				inst->index += inst->s[inst->i];
+			}
+		}
 		tmp += ft_strspn(tmp, SPACES);
 		tmp += ft_strcspn(tmp, SEPARATOR_CHAR);
-		if (inst->t[inst->i] && g_op_tab[inst->op - 1].nb_param > inst->i)
-		{
-			error_param_type(e, inst, begin);
-			inst->index += inst->s[inst->i];
-		}
 		inst->i++;
 		if (!*tmp)
 			break ;
 		if (*tmp++ == ',' && !tmp[ft_strspn(tmp, SPACES)])
+		{
 			basic_error(e, tmp - 1, "expected parameter after ','\n", 0);
+			inst->error++;
+		}
 	}
 	if (inst->i != g_op_tab[inst->op - 1].nb_param)
+	{
+		inst->error++;
 		error_nb_param(e, begin, inst->i, g_op_tab[inst->op - 1].nb_param);
+	}
 	return (1);
 }
