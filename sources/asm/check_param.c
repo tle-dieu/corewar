e7a9621772b0ffd51343129716197b8f2d32c268
Thomas Ubuntu <thomas.lddv@orange.fr>
2019-05-18 02:57:28 +0200

    comment norm

--- sources/asm/check_param.c
+++ sources/asm/check_param.c
@@ -6,7 +6,7 @@
 /*   By: matleroy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/09 15:07:09 by matleroy          #+#    #+#             */
-/*   Updated: 2019/05/18 02:09:05 by tle-dieu         ###   ########.fr       */
+/*   Updated: 2019/05/18 02:56:02 by tle-dieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,7 +74,7 @@ int		is_valid_register(t_env *e, char *str)
 	tmp += ft_strspn(tmp, SPACES);
 	if (e->file->error < MAX_ERROR && *tmp && *tmp != *SEPARATOR_CHAR)
 		err += basic_error(e, tmp, "unexpected expression after parameter\n",
-			ft_strcspn(tmp, END_PARAM) - 1);
+				ft_strcspn(tmp, END_PARAM) - 1);
 	return (!err);
 }
 
@@ -92,11 +92,11 @@ int		is_a_number(t_env *e, char *str)
 	if (e->file->error < MAX_ERROR && *tmp
 		&& *tmp != *SEPARATOR_CHAR && !ft_strchr(SPACES, *tmp))
 		err += basic_error(e, str, "invalid parameter\n",
-			param_strrspn(tmp, SPACES, *SEPARATOR_CHAR));
+				param_strrspn(tmp, SPACES, *SEPARATOR_CHAR));
 	tmp += ft_strcspn(tmp, END_PARAM);
 	tmp += ft_strspn(tmp, SPACES);
 	if (e->file->error < MAX_ERROR && *tmp && *tmp != *SEPARATOR_CHAR)
 		err += basic_error(e, tmp, "unexpected expression after parameter\n",
-			ft_strcspn(tmp, END_PARAM) - 1);
+				ft_strcspn(tmp, END_PARAM) - 1);
 	return (!err);
 }
