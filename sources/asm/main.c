0d177673512be9fe8c53e64527aa2f75d9535c28
Thomas LE DIEU DE VILLE <tle-dieu@e3r10p13.42.fr>
2019-06-08 14:37:18 +0200

    init env

--- sources/asm/main.c
+++ sources/asm/main.c
@@ -6,19 +6,31 @@
 /*   By: tle-dieu <tle-dieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/09 16:36:26 by tle-dieu          #+#    #+#             */
-/*   Updated: 2019/06/08 14:29:56 by tle-dieu         ###   ########.fr       */
+/*   Updated: 2019/06/08 14:34:48 by tle-dieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 #include <unistd.h>
 
-int		main(int ac, char **av)
+static void		init_env(t_env *e, char **av)
+{
+	e->tty1 = isatty(1);
+	e->tty2 = isatty(2);
+	e->i = 0;
+	e->actual = NULL;
+	e->file = NULL;
+	e->exname = av[0];
+	e->output = NULL;
+	e->d = NULL;
+}
+
+int				main(int ac, char **av)
 {
 	t_env	e;
 	t_file	*next;
 
-	e = (t_env){isatty(1), isatty(2), 0, NULL, NULL, av[0], NULL, NULL};
+	init_env(&e, av);
 	if (ac < 2)
 		return (usage(&e, 3));
 	if (!parse_command_line(&e, ac, av))
