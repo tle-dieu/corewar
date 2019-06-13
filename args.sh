3f3e1d97873c8d2cf08b66c5376adf90ccfe02e1
Thomas LE DIEU DE VILLE <tle-dieu@e3r6p16.42.fr>
2019-06-13 19:25:36 +0200

    fix scripts

--- args.sh
+++ args.sh
@@ -5,22 +5,24 @@ OPTIONS="zork.cor"
 FILE_NAME="*.cor"
 LOG_PATH="error/fsanitize.log"
 
-export ASAN_OPTIONS="log_path=$LOG_PATH color=always"
-rm -rf error
-mkdir error
-old=0
-percent=$(find $* -type f -name "$FILE_NAME" | wc -l)
-printf "0%%"
-while IFS= read -r -d $'\0'; do
-	timeout 5 ./$PROGRAM $OPTIONS $REPLY > /dev/null 2>&1 
-	nb_file=$(ls -1 error/ | wc -l)
-	if [[ $nb_file -gt $old ]]; then
-		new_file=$(ls error/ -Art | tail -n 1)
-		echo "./$PROGRAM $OPTIONS $REPLY" | cat - error/$new_file > temp && mv temp error/$new_file
-		old=$nb_file
-	fi
-	i=$((i + 1))
-	nb=$(echo "$i * 100 / $percent" | bc)
-	printf "\033[2K\r$nb%%"
-done < <(find $* -type f -name "$FILE_NAME" -print0)
-echo
+if [[ $# -gt 0 ]]; then
+	export ASAN_OPTIONS="log_path=$LOG_PATH color=always"
+	rm -rf error
+	mkdir error
+	old=0
+	percent=$(find $* -type f -name "$FILE_NAME" | wc -l)
+	printf "0%%"
+	while IFS= read -r -d $'\0'; do
+		timeout 5 ./$PROGRAM $OPTIONS $REPLY > /dev/null 2>&1 
+		nb_file=$(ls -1 error/ | wc -l)
+		if [[ $nb_file -gt $old ]]; then
+			new_file=$(ls -Art error/ | tail -n 1)
+			echo "./$PROGRAM $OPTIONS $REPLY" | cat - error/$new_file > temp && mv temp error/$new_file
+			old=$nb_file
+		fi
+		i=$((i + 1))
+		nb=$(echo "$i * 100 / $percent" | bc)
+		printf "\033[2K\r$nb%%"
+	done < <(find $* -type f -name "$FILE_NAME" -print0)
+	echo
+fi
--- asm_test_all_options.sh
+++ asm_test_all_options.sh
@@ -4,167 +4,169 @@ PROGRAM="asm"
 FILE_NAME="*"
 LOG_PATH="error/fsanitize.log"
 
-export ASAN_OPTIONS="log_path=$LOG_PATH color=always"
-rm -rf error
-mkdir error
-old=0
-printf "0%%"
-percent=$(echo "$(find $* -type f -name "$FILE_NAME" | wc -l) * 7 + 7" | bc)
-i=0
-while IFS= read -r -d $'\0'; do
-	./$PROGRAM -x $REPLY > /dev/null 2>&1 
+if [[ $# -gt 0 ]]; then
+	export ASAN_OPTIONS="log_path=$LOG_PATH color=always"
+	rm -rf error
+	mkdir error
+	old=0
+	printf "0%%"
+	percent=$(echo "$(find $* -type f -name "$FILE_NAME" | wc -l) * 7 + 7" | bc)
+	i=0
+	while IFS= read -r -d $'\0'; do
+		./$PROGRAM -x $REPLY > /dev/null 2>&1 
+		nb_file=$(ls -1 error/ | wc -l)
+		if [[ $nb_file -gt $old ]]; then
+			new_file=$(ls -Art error/ | tail -n 1)
+			echo "./$PROGRAM -x $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
+			old=$nb_file
+		fi
+		i=$((i + 1))
+		nb=$(echo "$i * 100 / $percent" | bc)
+		printf "\033[2K\r$nb%%"
+	
+		./$PROGRAM -xl $REPLY > /dev/null 2>&1 
+		nb_file=$(ls -1 error/ | wc -l)
+		if [[ $nb_file -gt $old ]]; then
+			new_file=$(ls -Art error/ | tail -n 1)
+			echo "./$PROGRAM -xl $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
+			old=$nb_file
+		fi
+		i=$((i + 1))
+		nb=$(echo "$i * 100 / $percent" | bc)
+		printf "\033[2K\r$nb%%"
+	
+		./$PROGRAM -b $REPLY > /dev/null 2>&1 
+		nb_file=$(ls -1 error/ | wc -l)
+		if [[ $nb_file -gt $old ]]; then
+			new_file=$(ls -Art error/ | tail -n 1)
+			echo "./$PROGRAM -b $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
+			old=$nb_file
+		fi
+		i=$((i + 1))
+		nb=$(echo "$i * 100 / $percent" | bc)
+		printf "\033[2K\r$nb%%"
+	
+		./$PROGRAM -bl $REPLY > /dev/null 2>&1 
+		nb_file=$(ls -1 error/ | wc -l)
+		if [[ $nb_file -gt $old ]]; then
+			new_file=$(ls -Art error/ | tail -n 1)
+			echo "./$PROGRAM -bl $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
+			old=$nb_file
+		fi
+		i=$((i + 1))
+		nb=$(echo "$i * 100 / $percent" | bc)
+		printf "\033[2K\r$nb%%"
+	
+		./$PROGRAM -o test.cor $REPLY > /dev/null 2>&1 
+		nb_file=$(ls -1 error/ | wc -l)
+		if [[ $nb_file -gt $old ]]; then
+			new_file=$(ls -Art error/ | tail -n 1)
+			echo "./$PROGRAM -o test.cor $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
+			old=$nb_file
+		fi
+		i=$((i + 1))
+		nb=$(echo "$i * 100 / $percent" | bc)
+		printf "\033[2K\r$nb%%"
+		rm -f test.cor
+	
+		./$PROGRAM -d $REPLY > /dev/null 2>&1 
+		nb_file=$(ls -1 error/ | wc -l)
+		if [[ $nb_file -gt $old ]]; then
+			new_file=$(ls -Art error/ | tail -n 1)
+			echo "./$PROGRAM -d $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
+			old=$nb_file
+		fi
+		i=$((i + 1))
+		nb=$(echo "$i * 100 / $percent" | bc)
+		printf "\033[2K\r$nb%%"
+	
+		./$PROGRAM $REPLY > /dev/null 2>&1 
+		nb_file=$(ls -1 error/ | wc -l)
+		if [[ $nb_file -gt $old ]]; then
+			new_file=$(ls -Art error/ | tail -n 1)
+			echo "./$PROGRAM $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
+			old=$nb_file
+		fi
+		i=$((i + 1))
+		nb=$(echo "$i * 100 / $percent" | bc)
+		printf "\033[2K\r$nb%%"
+	
+	done < <(find $* -type f -name "$FILE_NAME" -print0)
+	./$PROGRAM -color=a $REPLY > /dev/null 2>&1 
 	nb_file=$(ls -1 error/ | wc -l)
 	if [[ $nb_file -gt $old ]]; then
-		new_file=$(ls error/ -Art | tail -n 1)
-		echo "./$PROGRAM -x $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
+		new_file=$(ls -Art error/ | tail -n 1)
+		echo "./$PROGRAM -color=a $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
 		old=$nb_file
 	fi
 	i=$((i + 1))
 	nb=$(echo "$i * 100 / $percent" | bc)
 	printf "\033[2K\r$nb%%"
-
-	./$PROGRAM -xl $REPLY > /dev/null 2>&1 
+	
+	./$PROGRAM -color=f $REPLY > /dev/null 2>&1 
 	nb_file=$(ls -1 error/ | wc -l)
 	if [[ $nb_file -gt $old ]]; then
-		new_file=$(ls error/ -Art | tail -n 1)
-		echo "./$PROGRAM -xl $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
+		new_file=$(ls -Art error/ | tail -n 1)
+		echo "./$PROGRAM -color=f $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
 		old=$nb_file
 	fi
 	i=$((i + 1))
 	nb=$(echo "$i * 100 / $percent" | bc)
 	printf "\033[2K\r$nb%%"
-
-	./$PROGRAM -b $REPLY > /dev/null 2>&1 
+	
+	./$PROGRAM -color=forcea $REPLY > /dev/null 2>&1 
 	nb_file=$(ls -1 error/ | wc -l)
 	if [[ $nb_file -gt $old ]]; then
-		new_file=$(ls error/ -Art | tail -n 1)
-		echo "./$PROGRAM -b $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
+		new_file=$(ls -Art error/ | tail -n 1)
+		echo "./$PROGRAM -color=forcea $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
 		old=$nb_file
 	fi
 	i=$((i + 1))
 	nb=$(echo "$i * 100 / $percent" | bc)
 	printf "\033[2K\r$nb%%"
-
-	./$PROGRAM -bl $REPLY > /dev/null 2>&1 
+	
+	./$PROGRAM -color=al $REPLY > /dev/null 2>&1 
 	nb_file=$(ls -1 error/ | wc -l)
 	if [[ $nb_file -gt $old ]]; then
-		new_file=$(ls error/ -Art | tail -n 1)
-		echo "./$PROGRAM -bl $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
+		new_file=$(ls -Art error/ | tail -n 1)
+		echo "./$PROGRAM -color=al $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
 		old=$nb_file
 	fi
 	i=$((i + 1))
 	nb=$(echo "$i * 100 / $percent" | bc)
 	printf "\033[2K\r$nb%%"
-
-	./$PROGRAM -o test.cor $REPLY > /dev/null 2>&1 
+	
+	./$PROGRAM -color=n $REPLY > /dev/null 2>&1 
 	nb_file=$(ls -1 error/ | wc -l)
 	if [[ $nb_file -gt $old ]]; then
-		new_file=$(ls error/ -Art | tail -n 1)
-		echo "./$PROGRAM -o test.cor $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
+		new_file=$(ls -Art error/ | tail -n 1)
+		echo "./$PROGRAM -color=n $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
 		old=$nb_file
 	fi
 	i=$((i + 1))
 	nb=$(echo "$i * 100 / $percent" | bc)
 	printf "\033[2K\r$nb%%"
-	rm -f test.cor
-
-	./$PROGRAM -d $REPLY > /dev/null 2>&1 
+	
+	./$PROGRAM -color=autom $REPLY > /dev/null 2>&1 
 	nb_file=$(ls -1 error/ | wc -l)
 	if [[ $nb_file -gt $old ]]; then
-		new_file=$(ls error/ -Art | tail -n 1)
-		echo "./$PROGRAM -d $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
+		new_file=$(ls -Art error/ | tail -n 1)
+		echo "./$PROGRAM -color=autom $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
 		old=$nb_file
 	fi
 	i=$((i + 1))
 	nb=$(echo "$i * 100 / $percent" | bc)
 	printf "\033[2K\r$nb%%"
-
-	./$PROGRAM $REPLY > /dev/null 2>&1 
+	
+	./$PROGRAM -color=aut $REPLY > /dev/null 2>&1 
 	nb_file=$(ls -1 error/ | wc -l)
 	if [[ $nb_file -gt $old ]]; then
-		new_file=$(ls error/ -Art | tail -n 1)
-		echo "./$PROGRAM $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
+		new_file=$(ls -Art error/ | tail -n 1)
+		echo "./$PROGRAM -color=aut $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
 		old=$nb_file
 	fi
 	i=$((i + 1))
 	nb=$(echo "$i * 100 / $percent" | bc)
 	printf "\033[2K\r$nb%%"
-
-done < <(find $* -type f -name "$FILE_NAME" -print0)
-./$PROGRAM -color=a $REPLY > /dev/null 2>&1 
-nb_file=$(ls -1 error/ | wc -l)
-if [[ $nb_file -gt $old ]]; then
-	new_file=$(ls error/ -Art | tail -n 1)
-	echo "./$PROGRAM -color=a $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
-	old=$nb_file
+	echo
 fi
-i=$((i + 1))
-nb=$(echo "$i * 100 / $percent" | bc)
-printf "\033[2K\r$nb%%"
-
-./$PROGRAM -color=f $REPLY > /dev/null 2>&1 
-nb_file=$(ls -1 error/ | wc -l)
-if [[ $nb_file -gt $old ]]; then
-	new_file=$(ls error/ -Art | tail -n 1)
-	echo "./$PROGRAM -color=f $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
-	old=$nb_file
-fi
-i=$((i + 1))
-nb=$(echo "$i * 100 / $percent" | bc)
-printf "\033[2K\r$nb%%"
-
-./$PROGRAM -color=forcea $REPLY > /dev/null 2>&1 
-nb_file=$(ls -1 error/ | wc -l)
-if [[ $nb_file -gt $old ]]; then
-	new_file=$(ls error/ -Art | tail -n 1)
-	echo "./$PROGRAM -color=forcea $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
-	old=$nb_file
-fi
-i=$((i + 1))
-nb=$(echo "$i * 100 / $percent" | bc)
-printf "\033[2K\r$nb%%"
-
-./$PROGRAM -color=al $REPLY > /dev/null 2>&1 
-nb_file=$(ls -1 error/ | wc -l)
-if [[ $nb_file -gt $old ]]; then
-	new_file=$(ls error/ -Art | tail -n 1)
-	echo "./$PROGRAM -color=al $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
-	old=$nb_file
-fi
-i=$((i + 1))
-nb=$(echo "$i * 100 / $percent" | bc)
-printf "\033[2K\r$nb%%"
-
-./$PROGRAM -color=n $REPLY > /dev/null 2>&1 
-nb_file=$(ls -1 error/ | wc -l)
-if [[ $nb_file -gt $old ]]; then
-	new_file=$(ls error/ -Art | tail -n 1)
-	echo "./$PROGRAM -color=n $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
-	old=$nb_file
-fi
-i=$((i + 1))
-nb=$(echo "$i * 100 / $percent" | bc)
-printf "\033[2K\r$nb%%"
-
-./$PROGRAM -color=autom $REPLY > /dev/null 2>&1 
-nb_file=$(ls -1 error/ | wc -l)
-if [[ $nb_file -gt $old ]]; then
-	new_file=$(ls error/ -Art | tail -n 1)
-	echo "./$PROGRAM -color=autom $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
-	old=$nb_file
-fi
-i=$((i + 1))
-nb=$(echo "$i * 100 / $percent" | bc)
-printf "\033[2K\r$nb%%"
-
-./$PROGRAM -color=aut $REPLY > /dev/null 2>&1 
-nb_file=$(ls -1 error/ | wc -l)
-if [[ $nb_file -gt $old ]]; then
-	new_file=$(ls error/ -Art | tail -n 1)
-	echo "./$PROGRAM -color=aut $REPLY > /dev/null 2>&1" | cat - error/$new_file > temp && mv temp error/$new_file
-	old=$nb_file
-fi
-i=$((i + 1))
-nb=$(echo "$i * 100 / $percent" | bc)
-printf "\033[2K\r$nb%%"
-echo
