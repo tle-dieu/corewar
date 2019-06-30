18f72515a4c625421e8c968ed0a445c423066f29
Thomas LE DIEU DE VILLE <tle-dieu@e3r2p3.42.fr>
2019-06-30 19:50:05 +0200

    add script test

--- script/test.py
+++ script/test.py
@@ -6,25 +6,24 @@ import shutil
 import fnmatch
 import glob
 import subprocess as sp
+import argparse
 
 
-REPO = "../"
-OBJECTS_PATH = "objects"
-LIB_PATH = "libft"
-FILE_NAME_ASM = "*"
+# Compile corewar and libft with -fsanitize=address,undefined,integer,bounds
+
 LOG_PATH = "error/"
+LOG_FILE = "fsanitize.log"
+TIMEOUT = 10
+
+FILE_NAME_ASM = "*"
 LOG_PATH_ASM = LOG_PATH + "asm/"
 ASM_OPTIONS = ['-x', '-xl', '-b', '-bl', '-o test.cor', '-d', '']
 ASM_GLOBAL_OPTIONS = ['--color=f', '--color=a', '--color=forcea', '--color=al', '--color=n', '--color=aut']
-ASM_DEFAULT_CHAMP = "zork.s"
-COREWAR_SOURCE_CHAMP = "zork.s"
+
 FILE_NAME_COREWAR = "*.cor"
 LOG_PATH_COREWAR = LOG_PATH + "corewar/"
 COREWAR_OPTIONS = ['-verbose 0', '-verbose 1', '-verbose 2', '-verbose 3', '-verbose 4', '-dump 32', '-dump 4324324', '']
-COREWAR_GLOBAL_OPTIONS = ['-n 3', '-n 2334322343432', '-n -1', '-dump 0']
-COREWAR_DEFAULT_CHAMP = "zork.cor"
-LOG_FILE = "fsanitize.log"
-TIMEOUT = 10
+COREWAR_GLOBAL_OPTIONS = ['-n 3', '-n 2334322343432', '-n -1', '-dump 0', '$(find . -type f -name "*.cor")']
 
 def progress(count, total, status=''):
     bar_len = 60
@@ -33,7 +32,7 @@ def progress(count, total, status=''):
     bar = '=' * filled_len + '-' * (bar_len - filled_len)
     if count > 1:
         print("\033[2K", end='')
-    print('\r' + '[' + bar + ']' +  str(percents) + '% ' + status)
+    print('\r' + '[' + bar + '] ' +  str(percents) + '% ' + status, end="")
 
 def check_run(program, opt, latest_error, file, log_path):
     os.system('timeout ' + str(TIMEOUT) + ' ./' + program + ' ' + opt + ' ' + file + ' > /dev/null 2>&1')
@@ -63,9 +62,10 @@ def test_program(program, file_list, options, global_options, log_path):
                 i += 1
                 progress(i, percent, './' + program + ' ' + opt + ' ' + file)
         for opt in global_options:
-            latest_error = check_run(program, opt, latest_error, file, log_path)
+            latest_error = check_run(program, opt, latest_error, '', log_path)
             i += 1
-            progress(i, percent, './' + program + ' ' + opt + ' ' + file)
+            progress(i, percent, './' + program + ' ' + opt + ' ' + '')
+    print('')
 
 
 def get_files(directory, pattern):
@@ -85,85 +85,77 @@ def remove_if_exist(path):
             os.remove(path)
 
 
-def init_directory():
+def init_directory(args):
     remove_if_exist('tmp')
-    shutil.copytree(sys.argv[1], 'tmp')
+    shutil.copytree(args.champs, 'tmp')
+    args.champs = 'tmp'
     if not os.path.exists(LOG_PATH_ASM):
         os.makedirs(LOG_PATH_ASM)
     if os.path.exists('invalid_cor') and not os.path.isdir('invalid_cor'):
         os.remove('invalid_cor')
     if not os.path.exists('invalid_cor'):
         os.makedirs('invalid_cor')
-    file_list = get_files(sys.argv[1], '*.cor')
+    file_list = get_files(args.champs, '*.cor')
     for file in file_list:
-        shutil.copyfile(file, 'invalid_cor/')
-
-
-def init_executable():
+        shutil.copy(file, 'invalid_cor/')
     remove_if_exist('error')
     os.makedirs(LOG_PATH_ASM)
     os.makedirs(LOG_PATH_COREWAR)
-    file_list = get_files(OBJECTS_PATH, '*.o')
-    if LIB_PATH:
-        file_list.extend(get_files(LIB_PATH, '*.o'))
-    for file in file_list:
-        if not sp.getoutput('nm -u ' + file + ' | grep asan').strip():
-            remove_if_exist(file)
-    os.system('make -C ' + REPO + ' silent=y CFLAGS="-fsanitize=address,undefined,integer,bounds" > /dev/null 2>&1')
-    if not os.path.exists(REPO + 'asm') or not os.path.exists(REPO + 'corewar'):
+    if not os.path.exists(args.repo + 'asm') or not os.path.exists(args.repo + 'corewar'):
         print("Program not find")
         exit(1)
-    shutil.copyfile(REPO + 'asm', 'asm')
-    shutil.copyfile(REPO + 'corewar', 'corewar')
+    shutil.copy(args.repo + 'asm', 'asm')
+    shutil.copy(args.repo + 'corewar', 'corewar')
 
 
 def restore_directory(champs):
-    if os.path.exists('tmp'):
-        shutil.rmtree("tmp")
     remove_if_exist('invalid_cor')
     remove_if_exist(champs)
-    os.rename('tmp', champs)
 
 
-def asm_test(champs):
-    file_list = get_files(champs, FILE_NAME_ASM)
+def asm_test(args):
+    i = len(ASM_GLOBAL_OPTIONS)
+    while i > 0:
+        i -= 1
+        ASM_GLOBAL_OPTIONS[i] = ASM_GLOBAL_OPTIONS[i] + ' ' + args.asm_default_champ
+    file_list = get_files(args.champs, FILE_NAME_ASM)
     test_program('asm', file_list, ASM_OPTIONS, ASM_GLOBAL_OPTIONS, LOG_PATH_ASM)
 
 
-def corewar_test(champs):
-    os.system('./asm ' + COREWAR_DEFAULT_CHAMP + ' ' + COREWAR_SOURCE_CHAMP + ' > /dev/null')
-    shutil.move(os.path.splitext(COREWAR_DEFAULT_CHAMP)[0] + ".cor", COREWAR_SOURCE_CHAMP)
-    if os.path.exists(COREWAR_DEFAULT_CHAMP):
-        file_list = get_files(sys.argv[1], FILE_NAME_COREWAR)
+def corewar_test(args):
+    os.system('./asm ' + args.corewar_default_champ + ' > /dev/null')
+    default_cor = os.path.splitext(args.corewar_default_champ)[0] + ".cor"
+    if os.path.exists(default_cor):
+        file_list = get_files(args.champs, FILE_NAME_COREWAR)
         i = len(COREWAR_OPTIONS)
-        while i >= 0:
+        while i > 0:
             i -= 1
-            COREWAR_OPTIONS[i] =  COREWAR_OPTIONS[i] + ' ' + COREWAR_DEFAULT_CHAMP
-        while i >= 0:
+            COREWAR_OPTIONS[i] =  COREWAR_OPTIONS[i] + ' ' + default_cor
+        i = len(COREWAR_GLOBAL_OPTIONS)
+        while i > 0:
             i -= 1
-            COREWAR_GLOBAL_OPTIONS[i] = COREWAR_GLOBAL_OPTIONS[i] + ' ' + COREWAR_DEFAULT_CHAMP
+            COREWAR_GLOBAL_OPTIONS[i] = COREWAR_GLOBAL_OPTIONS[i] + ' ' + default_cor + ' ' + default_cor
         test_program('corewar', file_list, COREWAR_OPTIONS, COREWAR_GLOBAL_OPTIONS, LOG_PATH_COREWAR)
 
+def is_valid_file(parser, arg, dir):
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist" % arg)
+    elif os.path.isdir(arg) and dir is False:
+        parser.error("The file %s cannot be a directory" % arg)
+    elif not os.path.isdir(arg) and dir is True:
+        parser.error("The file %s must be a directory" % arg)
+    return arg
 
 def main():
-    if len(sys.argv) >= 2 and os.path.exists(sys.argv[1]):
-        init_directory()
-        print("init directory")
-        os.system('ls -R')
-        init_executable()
-        print("executable")
-        os.system('ls -R')
-        asm_test(sys.argv[1])
-        print("asm")
-        os.system('ls -R')
-        corewar_test(sys.argv[1])
-        print("corewar")
-        os.system('ls -R')
-        restore_directory(sys.argv[1])
-        print("restore directory")
-        os.system('ls -R')
-    else:
-        print("Usage: " + sys.argv[0] + ' <directory>')
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument("repo", help="repo directory", type=lambda x: is_valid_file(parser, x, True))
+    parser.add_argument("champs", help="directory containing champions", type=lambda x: is_valid_file(parser, x, True))
+    parser.add_argument("asm_default_champ", help="default champion for asm", type=lambda x: is_valid_file(parser, x, False))
+    parser.add_argument("corewar_default_champ", help="default champion for corewar", type=lambda x: is_valid_file(parser, x, False))
+    args = parser.parse_args()
+    init_directory(args)
+    asm_test(args)
+    corewar_test(args)
+    restore_directory(args.champs)
 
 main()
